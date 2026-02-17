7f123d5fc9598bf27c1ab18e045defafc4fa0483
KimSungJun <jt2302@naver.com>
2026-02-17 22:57:00 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/speakview/speakview/SpeakviewApplication.java
@@ -0,0 +1,13 @@
+package com.speakview.speakview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speakview/speakview/SpeakviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakview.speakview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
