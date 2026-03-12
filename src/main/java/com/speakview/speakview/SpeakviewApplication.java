c6e7b8eb5452132b8e6ae204ba48fd2944d1da1a
KimSungJun <jt2302@naver.com>
2026-03-13 01:25:29 +0900

    feat: 실시간 강의 내용 요약 구현 #26

--- src/main/java/com/speakview/speakview/SpeakviewApplication.java
+++ src/main/java/com/speakview/speakview/SpeakviewApplication.java
@@ -2,7 +2,9 @@ package com.speakview.speakview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // 타이머 기능 활성화
 @SpringBootApplication
 public class SpeakviewApplication {
 
