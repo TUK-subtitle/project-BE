bf7ef7a29a0aaead70242b4498b2047d831e5db2
KimSungJun <jt2302@naver.com>
2026-05-01 18:07:28 +0900

    refactor: Subject Entity refactoring #53

--- src/main/java/com/speakview/speakview/domain/user/entity/Subject.java
+++ src/main/java/com/speakview/speakview/domain/user/entity/Subject.java
@@ -1,11 +1,14 @@
 package com.speakview.speakview.domain.user.entity;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.speakview.speakview.domain.ai.entity.Content;
+import com.speakview.speakview.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,22 +23,28 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Subject {
+public class Subject extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "timeTableId", nullable = false)
+    @JoinColumn(name = "timetable_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private TimeTable timetable;
 
-    @Column(nullable = false, length = 20)
-    private String startTime;
+    @Column(name = "name")
+    private String name;
 
-    @Column(nullable = false)
-    private Integer progressTime;
+    @Column(name = "day_of_week")
+    private String dayOfWeek;
+
+    @Column(name = "start_time")
+    private LocalTime startTime;
+
+    @Column(name = "end_time")
+    private LocalTime endTime;
 
     @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Content> contents = new ArrayList<>();
